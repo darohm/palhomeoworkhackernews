c569b7075b9f862edb735fe287cc4c72c9f5a20f
David Park <david.j.park@ieee.org>
2019-05-10 15:49:39 -0700

    Changed project space from pal-tracker to pal-hackernews.

--- src/main/java/io/pivotal/pal/hackernews/HackerNewsApplication.java
+++ src/main/java/io/pivotal/pal/hackernews/HackerNewsApplication.java
@@ -1,12 +1,12 @@
-package io.pivotal.pal.tracker;
+package io.pivotal.pal.hackernews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PalTrackerApplication {
+public class HackerNewsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PalTrackerApplication.class, args);
+        SpringApplication.run(HackerNewsApplication.class, args);
     }
 }
